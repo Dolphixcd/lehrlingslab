1912364cf0cf5c041a40efa25de42e2c55aabe61
Dolphix <166293610+Dolphixcd@users.noreply.github.com>
2024-07-23 08:06:14 +0200

    chore: Add sidebar navigation component
    The code changes include:
    - Adding a new component for the sidebar navigation
    - Updating the app component to import the sidebar navigation component
    - Adding HTML and CSS files for the sidebar navigation component
    
    This commit adds the necessary code for implementing a sidebar navigation feature in the application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarNavComponent } from "./sidebar-nav/sidebar-nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
